dc353c4b000dc18d81d220c040221f1b8b59e4ea
dienthong <96158240+Hoanglam111@users.noreply.github.com>
2023-07-02 22:33:29 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+app.use(express["static"]("public"));
+const connectedClients = {};
+io.on("connection", (socket) => {
+  console.log("Một client đã kết nối");
+  connectedClients[socket.id] = { socket: socket };
+  socket.on("message", (data) => {
+    console.log("Nhận tin nhắn từ client:", data);
+    io.emit("message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("Một client đã ngắt kết nối");
+    delete connectedClients[socket.id];
+  });
+});
+const port = 3e3;
+server.listen(port, () => {
+  console.log(`Máy chủ đang lắng nghe tại http://localhost:${port}`);
+});
+
